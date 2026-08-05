f3785cdbdafd78649678cec91c61db04585d9d47
mrkmc1421-netizen <mrkmc1421@gmail.com>
2026-08-05 19:03:42 -0400

    Add Editor.svelte for sprite management

---
+++ Editor.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+  import Sprite1 from "./Sprite1.svelte";
+
+  let sprites = [];
+  sprites.push({
+    id: "sprite1",
+    name: "Dodo",
+    x: 200,
+    y: 200,
+    currentCostume: 0,
+    costumes: [
+      {
+        id: "dodo-default",
+        name: "Dodo Mascot",
+        url: "/assets/dodo.png"
+      }
+    ]
+  });
+</script>
+
+{#each sprites as s}
+  <Sprite1 sprite={s} />
+{/each}
